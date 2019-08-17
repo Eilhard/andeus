e958415ca9243ff2b5b240db0183b84a0c62d89d
Yevhen <psyvit@gmail.com>
2019-08-17 15:53:00 +0300

    nav btns

--- src/app/components/app.vue
+++ src/app/components/app.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="app-container">
     <nav class="global-nav">
-      <button class="global-nav__button">Editor</button>
-      <button class="global-nav__button">Bestiary</button>
+      <button v-on:click="navButtons('Bestiary')" class="global-nav__button">Bestiary</button>
+      <button v-on:click="navButtons('Editor')" class="global-nav__button">Editor</button>
     </nav>
-
-    <bestia-card v-for="bestia in besties" :key="bestia.id"
-      :name="bestia.name"
-      :race="bestia.race"
-      :lvl="bestia.lvl"
-      :hp="bestia.hp"
-      :energy="bestia.energy"
-      :loot="bestia.loot">
-    </bestia-card>
+    <div v-show="buttons.bestiary" class="app-container__card-holder">
+      <bestia-card v-for="bestia in besties" :key="bestia.id"
+        :name="bestia.name"
+        :race="bestia.race"
+        :lvl="bestia.lvl"
+        :hp="bestia.hp"
+        :energy="bestia.energy"
+        :loot="bestia.loot">
+      </bestia-card>
+    </div>
+    <div v-show="buttons.editor" class="app-container__card-holder">
+      <form class="">
+        <input type="text">
+      </form>
+    </div>
   </div>
 </template>
 
@@ -24,6 +30,10 @@
     data() {
       return {
         besties: null,
+        buttons: {
+          editor: false,
+          bestiary: true
+        }
       }
     },
     mounted() {
@@ -32,6 +42,9 @@
       });
     },
     methods: {
+      navButtons(btnName) {
+        alert(btnName);
+      },
       syncBesties() {
         axiBeast('bestia').then(res => {
           this.besties = res.data;
@@ -50,6 +63,12 @@
     display: flex;
     justify-content: center;
     flex-direction: column;
+
+    &__card-holder {
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+    }
   }
 
   .global-nav {
@@ -62,29 +81,30 @@
     height: 60px;
     color: #FFFFFF;
     background-color: #323232;
-    @media (max-width: 650px) {
+    @media (max-width: 400px) {
       height: auto;
     }
     &__button {
       width: 100px;
-      height: 25px;
-      margin-right: 10px;
+      height: 100%;
       font-family: monospace;
       font-size: 16px;
       font-weight: bold;
       text-transform: uppercase;
       border: none;
-      color: #323232;
-      background-color: #80CBC4;
-      border-radius: 3px;
+      color: #747474;
+      background-color: #323232;
+      &--pressed {
+        color: #FFFFFF;
+      }
       &:hover {
-        background-color: darken(#80CBC4, 10%);
+        color: #FFFFFF;
+        background-color: darken(#323232, 5%);
         cursor: pointer;
       }
-      @media (max-width: 650px) {
-        width: 90%;
-        margin: 0 5% 0 5%;
-        margin-top: 0;
+      @media (max-width: 400px) {
+        width: 100%;
+        height: 40px;
       }
     }
   }
