159040bc09317796f1223d835498fa5ff870030a
Eilhard <psyvit@gmail.com>
2019-09-28 16:52:51 +0300

    remove test_api

--- backend/test_api/functions/datafunc.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-module.exports.saveLocalData = function(file, key, dataArray) {
-  let newData = JSON.stringify({ [key]: dataArray })
-  fs.writeFile(file, newData, error => { if(error) throw error; });
-};
